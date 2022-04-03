677c38a5f80d542fd9e24b1b339c4b42101e659b
pablo <pablo774@protonmail.com>
2022-04-03 03:57:29 +0200

    .

--- datazaur/settings.py
+++ datazaur/settings.py
@@ -141,13 +141,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static', 'js'),
-    os.path.join(BASE_DIR, 'static', 'css'),
-    os.path.join(BASE_DIR, 'static', 'img')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
