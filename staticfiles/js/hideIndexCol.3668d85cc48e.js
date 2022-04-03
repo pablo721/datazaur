74e78bb707f7069b6875a51a3a587da4f39a11c5
pablo <pablo774@protonmail.com>
2022-04-03 04:00:12 +0200

    .

--- staticfiles/js/hideIndexCol.3668d85cc48e.js
+++
@@ -1,8 +0,0 @@
-
-let tables = document.getElementsByTagName('table');
-
-for (let table of tables){
-    for (let row of table.rows){
-        row.deleteCell(0);
-    }
-}
--- staticfiles/js/toggleNavbar.8cda81a53237.js
+++
@@ -1,7 +0,0 @@
-
-
-
-function toggleNavbar(){
-    let navbar = document.getElementById('navbar');
-
-    };
