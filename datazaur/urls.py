34f6d5438568008ffb16a6b8a4f854ff8db40bbf
pablo <pablo774@protonmail.com>
2022-05-14 19:12:31 +0200

    rework

--- datazaur/urls.py
+++ datazaur/urls.py
@@ -2,15 +2,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crypto/', include('crypto.urls'), name='crypto'),
     path('markets/', include('markets.urls'), name='markets'),
-    path('data/', include('data.urls'), name='data'),
-    path('news/', include('news.urls'), name='news'),
+    path('crypto/', include('crypto.urls'), name='crypto'),
+    path('trade/', include('trade.urls'), name='trade'),
+    path('watchlist/', include('watchlist.urls'), name='watchlist'),
+    path('portfolio/', include('portfolio.urls'), name='portfolio'),
     path('trends/', include('trends.urls'), name='trends'),
+    path('news/', include('news.urls'), name='news'),
+    path('calendar/', include('_calendar.urls'), name='calendar'),
+    path('macro/', include('macro.urls'), name='macro'),
     path('messenger/', include('messenger.urls'), name='messenger'),
     path('', include('website.urls'), name='website'),
 ]
+
 
